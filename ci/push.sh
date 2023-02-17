bfa6e0530da15526aa7c8b13d3604a4025bd56ef
chaan2835 <92902854+chaan2835@users.noreply.github.com>
2023-02-17 11:38:34 +0530

    Update push.sh

--- ci/push.sh
+++ ci/push.sh
@@ -5,5 +5,5 @@ echo "ls -lrt"
 ls -lh 
 artifact_file_name=`ls *.war`
 echo "file name: $artifact_file_name"
-curl -u${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} -X PUT "http://3.6.89.240:8081/artifactory/libs-snapshot/${JOB_NAM}-${artifact_file_name}" -T ${artifact_file_name}
+curl -u${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} -X PUT "http://3.6.89.240:8081/artifactory/libs-snapshot/$JOB_NAME/$artifact_file_name" -T $artifact_file_name
 
